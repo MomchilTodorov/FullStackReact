855f5bb809938a4e02027d564bbe83456473ccbc
MomchilTodorov <52608910+MomchilTodorov@users.noreply.github.com>
2020-11-22 23:30:51 +0200

    Update Landing.js

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,41 +6,36 @@ class Landing extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return;
+        return <span>Loading...</span>;
       case false:
         return (
-          <p
-            className="center-align flow-text"
-            style={{
-              fontWeight: "bold",
-              color: "#263238",
-              paddingTop: "50px",
-            }}
-          >
+          <span>
             Login and create a survey <br />
             to collect feedback from your users!
-          </p>
+          </span>
         );
       default:
         return (
-          <p
-            className="center-align flow-text"
-            style={{
-              fontWeight: "bold",
-              color: "#263238",
-              paddingTop: "50px",
-            }}
-          >
+          <span>
             Welcome back! <br />
             Click on Surveys to continue!
-          </p>
+          </span>
         );
     }
   }
   render() {
     return (
       <div className="container" style={{ backgroundColor: "#62b8ea" }}>
-        {this.renderContent()}
+        <p
+          className="center-align flow-text"
+          style={{
+            fontWeight: "bold",
+            color: "#263238",
+            paddingTop: "50px",
+          }}
+        >
+          {this.renderContent()}
+        </p>
         <Image
           src="https://www.upvoty.com/wp-content/uploads/2020/01/best-ways-to-collect-user-feedback.png"
           aspectRatio="1/2"
